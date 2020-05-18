bfb5a0800598094472940d9a409203e7b663baca
Andrew Reyes-Cairo <andyrc6@gmail.com>
2020-05-18 11:10:29 -0600

    finished, added key signing data

--- components/Login.js
+++ components/Login.js
@@ -1,12 +1,52 @@
-import React, {} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-// import { } from 'react-native-elements';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
+import { Overlay, Input, Icon, Button } from 'react-native-elements';
 
 function Login(){
+    //set up states to hold the username and pw text
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const authenticate = () => {
+        //normally, this is where we'd send our data off to be authenticated. 
+        alert('You will now be authenticated.');
+    }
 
     return (
         <View style={styles.container}>
-                <Text style={styles.loginText}>Hello World</Text>
+            <Overlay isVisible={true} overlayStyle={styles.overlay}>
+                <>
+                <Image 
+                style={{alignSelf:'center', padding:0, margin:0}}
+                resizeMode="contain" 
+                source={require('../public/images/logo-5.png')} 
+                />
+                <Input
+                value={username}
+                placeholder='Username'
+                keyboardAppearance="dark"
+                leftIcon={<Icon name="user" type="font-awesome" color="lightgrey"/>}
+                onChangeText={(val) => setUsername(val)}
+                />
+
+                <Input
+                value={password}
+                placeholder='Password'
+                keyboardAppearance="dark"
+                secureTextEntry
+                leftIcon={<Icon name="lock" type="font-awesome" color="lightgrey"/>}
+                onChangeText={(val) => setPassword(val)}
+                />
+
+                <Button 
+                title="Sign In"
+                raised
+                type="outline"
+                onPress={() => authenticate()}
+                />
+                
+                </>
+            </Overlay>
         </View>
     )
 }
@@ -18,6 +58,9 @@ const styles = StyleSheet.create({
     },
     loginText: {
         textAlign:'center'
+    },
+    overlay: {
+        width: Dimensions.get('window').width - 20
     }
 })
 
